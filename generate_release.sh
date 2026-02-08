6d68b30d4d1f93089ad932a210b00f4a4ee8af62
BOSSe <BOSSe@danielnylander.se>
2026-02-08 14:08:21 +0100

    Update linguaedit to 0.4.1

---
+++ generate_release.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+cd dists/stable
+
+# Generate checksums
+do_hash() {
+    HASH_NAME=$1
+    HASH_CMD=$2
+    echo "${HASH_NAME}:"
+    for f in $(find -type f ! -name "Release*" ! -name "InRelease"); do
+        f=$(echo $f | cut -c3-)
+        SIZE=$(wc -c < "$f" | tr -d ' ')
+        HASH=$($HASH_CMD "$f" | cut -d' ' -f1)
+        echo " ${HASH} ${SIZE} ${f}"
+    done
+}
+
+cat << RELEASE
+Origin: Yeager
+Label: Yeager's Translation Tools
+Suite: stable
+Codename: stable
+Version: 1.0
+Architectures: amd64 arm64
+Components: main
+Description: Yeager's Translation Tools APT Repository
+Date: $(date -Ru)
+$(do_hash "MD5Sum" "openssl dgst -md5 -r")
+$(do_hash "SHA1" "openssl dgst -sha1 -r")
+$(do_hash "SHA256" "openssl dgst -sha256 -r")
+RELEASE
